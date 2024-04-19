d1884206206b712703e3d4fdd569a61574eaab9a
wesam-c <wesameldeeb15@gmail.com>
2024-04-19 23:08:05 +0400

    add

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#creating a file name school inside /tmp directory
+file { '/tmp/school':
+    ensure  =>  file,
+    content  =>  'I love Puppet',
+    mode  =>  '0744',
+    owner  =>  'www-data',
+    group  =>  'www-data',
+}
