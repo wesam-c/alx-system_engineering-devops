07be0ef62573ad0d9d93fc93f0ba5db09b2ec6f6
wesam-c <115662533+wesam-c@users.noreply.github.com>
2024-04-20 00:23:15 +0400

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-#creating a file name school inside /tmp directory
-file { '/tmp/school':
-    ensure   =>  file,
-    content  =>  'I love Puppet',
-    mode     =>  '0744',
-    owner    =>  'www-data',
-    group    =>  'www-data',
-}
