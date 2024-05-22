3054d7d142a37b42b341d1161a893c7246bdef5b
wesam-c <wesameldeeb15@gmail.com>
2024-05-22 20:19:37 +0400

    edit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
-'''
-gather employee data from API
-'''
-
-import re
+"""Gather data from an API"""
 import requests
 import sys
 
-if __name__ == '__main__':
-    user = sys.argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-    res = requests.get(url_user)
-    """ANYTHING"""
-    user_name = res.json().get('username')
-    task = url_user + '/todos'
-    res = requests.get(task)
-    tasks = res.json()
 
-    with open('{}.csv'.format(user), 'w') as csvfile:
-        for task in tasks:
-            completed = task.get('completed')
-            """Complete"""
-            title_task = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, user_name, completed, title_task))
-            
+def get_user_name(user_id):
+    """Get the list of users"""
+    url = "https://jsonplaceholder.typicode.com"
+    endpoint = "users"
+    """Format the url"""
+    url_to_call = f"{url}/{endpoint}/{user_id}"
+    """Get the response"""
+    response = requests.get(url_to_call)
+    json_response = response.json()
+    return json_response
+
+
+def get_user_todos(user_id):
+    """Get the list of todos"""
+    url = "https://jsonplaceholder.typicode.com"
+    endpoint = "todos"
+    """Format the url"""
+    url_to_call = f"{url}/users/{user_id}/{endpoint}"
+    """Get the response"""
+    response = requests.get(url_to_call)
+    json_response = response.json()
+    return json_response
+
+
+def export_csv(todos, user_id, name):
+    """Export to CSV"""
+    with open(f"{user_id}.csv", "w") as file:
+        for todo in todos:
+            file.write(f'"{user_id}","{name}",'
+                       f'"{todo.get("completed")}","{todo.get("title")}"\n')
+
+
+if __name__ == "__main__":
+    """Get User TODOS"""
+    user_id = sys.argv[1]
+    user_todos = get_user_todos(user_id)
+    user = get_user_name(user_id)
+    export_csv(user_todos, user_id, user.get("username"))
+
+    
